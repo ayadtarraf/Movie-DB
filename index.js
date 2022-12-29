8f74b0b389702ce23753ec9c2c5c72d7f3eec191
ayadtarraf <tarraf_tarraf1111@hotmail.com>
2022-12-29 12:38:37 +0200

    step 4

--- index.js
+++ index.js
@@ -17,7 +17,22 @@ app.get("/time", (req, res) => {
   res.send({ status: 200, message: editedTime });
 });
 
+app.get("/hello/:id", (req, res) => {
+  const id = req.params.id;
+  res.send({ status: 200, message: `hello, ${id}` });
+});
+
+app.get("/search", (req, res) => {
+  const search = req.query.s;
+  if (search) {
+    res.send({ status: 200, message: "ok", data: search });
+  } else {
+    res.status(500)
+    res.send({status:500, error:true, message:"you have to provide a search"})
+  }
+  
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
-
