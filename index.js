531b97d239471fdf75941c15a5a48fc713f38523
ayadtarraf <tarraf_tarraf1111@hotmail.com>
2022-12-29 15:07:58 +0200

    step 5

--- index.js
+++ index.js
@@ -27,11 +27,39 @@ app.get("/search", (req, res) => {
   if (search) {
     res.send({ status: 200, message: "ok", data: search });
   } else {
-    res.status(500)
-    res.send({status:500, error:true, message:"you have to provide a search"})
+    res.status(500);
+    res.send({
+      status: 500,
+      error: true,
+      message: "you have to provide a search",
+    });
   }
-  
 });
+app.get("/movies/create", (req, res) => {
+  const id = req.params.id;
+  res.send({ status: 200, message: `hello, ${id}` });
+});
+
+app.get("/movies/read", (req, res) => {
+  res.send({status:200, data:movies });
+});
+
+app.get("/movies/update", (req, res) => {
+  const id = req.params.id;
+  res.send({ status: 200, message: `hello, ${id}` });
+});
+
+app.get("/movies/delete", (req, res) => {
+  const id = req.params.id;
+  res.send({ status: 200, message: `hello, ${id}` });
+});
+
+const movies = [
+    { title: 'Jaws', year: 1975, rating: 8 },
+    { title: 'Avatar', year: 2009, rating: 7.8 },
+    { title: 'Brazil', year: 1985, rating: 8 },
+    { title: 'الإرهاب والكباب', year: 1992, rating: 6.2 }
+]
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
